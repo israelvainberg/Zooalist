b96493a2a48d0d6228779aa23b96656e683b64bb
Israel Vainberg <israelvainberg@gmail.com>
2019-05-12 16:20:45 +0300

    Added staging db

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['development'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['staging'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'barshemc_zoo',
+	'password' => '1zoo2alist3israel',
+	'database' => 'barshemc_zooalist',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8mb4',
+	'dbcollat' => 'utf8mb4_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
